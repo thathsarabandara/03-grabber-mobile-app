cf515c37d75e18f56ec69a787b39a786275a6d88
Thathsara Bandara <thathsaraarumapperuma@gmail.com>
2026-06-10 21:17:28 +0530

    chore: migrate lucide_icons dependency

--- lib/features/dashboard/screens/main_shell.dart
+++ lib/features/dashboard/screens/main_shell.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:lucide_icons/lucide_icons.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
 
 class MainShell extends StatefulWidget {
   final Widget child;
--- lib/features/dashboard/screens/notification_details_screen.dart
+++ lib/features/dashboard/screens/notification_details_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:lucide_icons/lucide_icons.dart';
+import 'package:lucide_icons_flutter/lucide_icons.dart';
 
 class NotificationDetailsScreen extends StatelessWidget {
   final String title;
